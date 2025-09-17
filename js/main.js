2148f181a0693a338211fa671dc7f7cc02ab9de0
FFunFForever <232723541+FFunFForever@users.noreply.github.com>
2025-09-17 20:37:06 +0100

    del

--- js/main.js
+++
@@ -1,2 +0,0 @@
-const y=document.getElementById('year');if(y) y.textContent=new Date().getFullYear();
-const toggle=document.getElementById('themeToggle');toggle?.addEventListener('click',()=>{document.body.classList.toggle('theme-light');document.body.classList.toggle('theme-dark');});

e2575a7182d205d108ee3e9ff483d6aca4818f81
FFunFForever <funforever5969@gmail.com>
2025-09-18 01:17:51 +0100

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-// assets/js/main.js
-document.getElementById('year').textContent = new Date().getFullYear();
-const menuBtn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.nav');
-if (menuBtn) menuBtn.addEventListener('click', () => nav.classList.toggle('open'));
